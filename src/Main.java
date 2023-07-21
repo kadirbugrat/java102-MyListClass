5c7b1fcaea0d72e2501be87e683c8fe7cd3adb2c
Kadir Bugra <kadirbugrat@gmail.com>
2023-07-21 17:19:34 +0300

    week-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyList<Integer> myList = new MyList<>();
+
+
+        for (int i = 1; i <= 15; i++) {
+            myList.add(i);
+        }
+
+
+        System.out.println("Dizinin eleman sayısı: " + myList.size());
+        System.out.println("Dizinin kapasitesi: " + myList.getCapacity());
+
+
+        System.out.print("Dizideki elemanlar: ");
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print(myList.get(i) + " ");
+        }
     }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class MyList {
+public class MyList<T> {
+    private T[] array;
+    private int size;
+    private int capacity;
+
+    public MyList() {
+        this(10);
+    }
+
+    public MyList(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be greater than zero.");
+        }
+        this.array = (T[]) new Object[capacity];
+        this.size = 0;
+        this.capacity = capacity;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void add(T data) {
+        if (size == capacity) {
+            increaseCapacity();
+        }
+        array[size] = data;
+        size++;
+    }
+
+    private void increaseCapacity() {
+        int newCapacity = capacity * 2;
+        T[] newArray = (T[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+        capacity = newCapacity;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds: " + index);
+        }
+        return array[index];
+    }
 }
